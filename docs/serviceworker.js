efb34dee39ff561c2201f9ee394b519f7680f85d
Lisa Demmer <ldemmer6@gmail.com>
2019-02-25 11:36:32 +0100

    change name folder for pwa

---
+++ docs/serviceworker.js
@@ -0,0 +1,62 @@
+var cacheName = 'break-it';
+var filesToCache = [
+  './',
+  './data',
+  './files',
+  './images',
+  './plugins',
+  './resources/scripts/jquery-1.7.1.min',
+  './menu_home.html',
+  './menu_level.html',
+  './menu_options.html',
+  './game.html',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('beforeinstallprompt', function(e) {
+  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+  e.userChoice.then(function(choiceResult) {
+
+    console.log(choiceResult.outcome);
+
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    }
+    else {
+      console.log('User added to home screen');
+    }
+  });
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+//cache before
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
