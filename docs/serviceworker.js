2411f68b16f6402a07bbf29bd0b660cd77d39401
Lisa Demmer <ldemmer6@gmail.com>
2019-03-04 08:35:04 +0100

    update serviceworker

--- docs/serviceworker.js
+++ docs/serviceworker.js
@@ -1,24 +1,22 @@
 var cacheName = 'break-it';
 var filesToCache = [
-  './',
-  './resources/scripts/jquery-1.7.1.min',
-  './index.html',
-  './menu_level.html',
-  './menu_options.html',
-  './game.html',
+  '/',
+  '/resources/scripts/jquery-1.7.1.min',
+  '/index.html',
+  '/menu_level.html',
+  '/menu_options.html',
+  '/game.html',
 ];
 
-self.addEventListener('install', function(e) {
+this.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
-  //e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     });
-  //);
 });
 
-self.addEventListener('beforeinstallprompt', function(e) {
+this.addEventListener('beforeinstallprompt', function(e) {
   // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
   e.userChoice.then(function(choiceResult) {
 
@@ -33,7 +31,7 @@ self.addEventListener('beforeinstallprompt', function(e) {
   });
 });
 
-self.addEventListener('activate', function(e) {
+this.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
@@ -45,10 +43,10 @@ self.addEventListener('activate', function(e) {
       }));
     })
   );
-  return self.clients.claim();
+  return this.clients.claim();
 });
 //cache before
-self.addEventListener('fetch', function(e) {
+this.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
